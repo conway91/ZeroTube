628b83d782b822f74691a365f24b23485a2840cc
Martin Conway <m_conway@live.co.uk>
2021-03-06 14:51:23 +0000

    Added additional styling and header/footer

--- frontend-app/src/components/YouTubeVideo.js
+++ frontend-app/src/components/YouTubeVideo.js
@@ -1,16 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react';
+import './YouTubeVideo.css'
 
-const YouTubeVideo = ({ videoInfo }) => {
-    return (
-        <div>
-            <h1>YouTube Video</h1>
-             <p>Video Id : { videoInfo['VideoId'] }</p>
-            <iframe id="youtubeVideo" title="youtubeVideo" width="640" height="360"
-    src={"https://www.youtube.com/embed/" + videoInfo['VideoId'] + "?autoplay=1"}
-    frameBorder="0"/>
-             <p>Video views : { videoInfo['ViewCount'] }</p>
-        </div>
-    )
-};
+class YouTubeVideo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            videoInfo: ""
+        };
+    }
+
+    componentDidMount() {
+        // fetch('https://9jt0cb12a9.execute-api.eu-west-1.amazonaws.com/zerotube/random')
+        //     .then(res => res.json())
+        //     .then((data) => {
+        //         this.setState({ videoInfo: data })
+        //     })
+        //     .catch(console.log)
+
+        // Uncomment for testing without hitting the API
+        this.setState({ videoInfo: {
+                VideoId: "BY8KsQFmyUM",
+                ViewCount: "99"
+        }})
+    }
+
+    render() {
+        return (
+            <div className="youtube-video-container">
+                <iframe id="youtubeVideo" title="youtubeVideo" width="640" height="360"
+                        src={"https://www.youtube.com/embed/" + this.state.videoInfo['VideoId'] + "?autoplay=1"}
+                        frameBorder="0"/>
+                <p>Video views : { this.state.videoInfo['ViewCount'] }</p>
+            </div>
+        );
+    }
+}
 
 export default YouTubeVideo
--- frontend-app/src/index.js
+++ frontend-app/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import YouTubeVideo from './components/YouTubeVideo';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Header />
+      <YouTubeVideo />
+      <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
